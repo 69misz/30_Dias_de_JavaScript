983d8913cc0dc0a2778d616f579879f97d0000d9
69misz <132626893+69misz@users.noreply.github.com>
2023-05-31 01:12:13 -0400

    Update ejercicio dia1.js

--- ejercicio dia1.js
+++ ejercicio dia1.js
@@ -8,10 +8,7 @@
 
 // 3. Escribe un comentario multilínea que diga, "Los comentarios pueden hacer el código legible, fácil de usar e informativo"
 
-    /* Los comentarios pueden hacer el 
-        let código_legible
-       let fácil_de_usar
-       let informativo */
+    /* "Los comentarios pueden hacer el código legible, fácil de usar e informativo" */
 
 // 4. Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
 // 5. Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
